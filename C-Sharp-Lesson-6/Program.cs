55b6d130eaa97c1cf3c76ce0aef42803aa7a8946
ProgrammingPark <90354391+ProgrammingPark@users.noreply.github.com>
2021-10-05 00:42:26 +0300

    Add the 6th C# lesson

---
+++ C-Sharp-Lesson-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixthLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter temperature: ");
+            int temp = int.Parse(Console.ReadLine());
+
+            //if (temp > 25)
+            //    Console.WriteLine("Temp is perfect!");
+            //else if (temp == 20)
+            //    Console.WriteLine("Temp is good!");
+            //else
+            //    Console.WriteLine("Temp is cold!");
+
+            //bool isCold = false;
+
+            //if (!isCold)
+            //    Console.WriteLine("Temp is good!");
+
+            Console.WriteLine($"Temp is {(temp > 25 ? "Good" : "Cold")}");
+
+            Console.ReadKey();
+        }
+    }
+}
