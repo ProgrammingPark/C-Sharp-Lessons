59d67a8547f62ec3c02d924f270785cf6d4b055f
ProgrammingPark <90354391+ProgrammingPark@users.noreply.github.com>
2021-11-07 20:43:00 +0200

    Added the 7th C# lesson

---
+++ C-Sharp-Lesson-7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SeventhLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 10;
+            int secondNumber = 3;
+            int thirdNumber = 5;
+
+            Console.WriteLine(number * secondNumber / thirdNumber);
+            Console.ReadKey();
+        }
+    }
+}
