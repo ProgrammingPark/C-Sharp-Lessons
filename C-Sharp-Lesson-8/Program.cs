0a5e08c8a2e41727eb62115e43a0fc85ffce023d
ProgrammingPark <90354391+ProgrammingPark@users.noreply.github.com>
2021-12-05 19:57:52 +0200

    Add the 8th C# lesson

---
+++ C-Sharp-Lesson-8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EightLesson
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.Write("Today:");
+            int temperature = int.Parse(Console.ReadLine());
+            Console.Write("Prev day:");
+            int prevDayTemperature = int.Parse(Console.ReadLine());
+
+            switch ((temperature, prevDayTemperature))
+            {
+                case (> 0, > 0) when temperature == prevDayTemperature:
+                    return 0;
+                case (> 0, > 0):
+                    return 1;
+                default:
+                    return 2;
+            }
+        }
+    }
+}
