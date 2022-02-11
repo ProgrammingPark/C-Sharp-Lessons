d81967b724353b5f619cb2169ec6bf3fe672084b
ProgrammingPark <90354391+ProgrammingPark@users.noreply.github.com>
2022-02-12 00:14:30 +0200

    Add the 9th C# lesson

---
+++ C-Sharp-Lesson-9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0, Console.WriteLine("Cycle start"); i < 5; i++, Console.WriteLine(i))
+            {
+                //if (i == 2)
+                //    break;
+
+                if (i % 2 == 0)
+                    continue;
+            }
+
+            int n = 0;
+
+            do
+            {
+                Console.WriteLine("Do");
+                n++;
+            }
+            while (n < 0);
+
+            n = 0;
+
+            while (n < 0)
+            {
+                Console.WriteLine("While");
+                n++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
